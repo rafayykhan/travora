ee532918b6442f0ff3cf972f59d3a8d37352f0c4
Rafay Khan <131152047+rafayykhan@users.noreply.github.com>
2026-07-26 17:06:04 +0500

    feat: add initial project setup with React, Vite, and Tailwind CSS
    - Added SVG assets for React and Vite.
    - Created Navbar component for navigation.
    - Set up main entry point with React Router.
    - Configured Tailwind CSS with base, components, and utilities.
    - Created tailwind.config.js for Tailwind CSS configuration.
    - Added vite.config.js for Vite configuration.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Routes } from 'react-router-dom'
+import './App.css'
+import './index.css'
+import Navbar from './components/Navbar.jsx'
+// import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+
+    return (
+        <nav className='NavbarItems'>
+            <h1 className = 'nav-bar logo'> Travora</h1>
+            <ul className='nav-menu'>
+                <li>
+                    <Link to ='/Home'>
+                    <i className="fa-solid fa-house"></i>Home
+                    </Link>
+                    <Link to ='/About'>
+                    <i className="fa-solid fa-circle-info"></i>About
+                    </Link>
+                    <Link to ='/Services'>
+                    <i className="fa-solid fa-briefcase"></i>Services
+                    </Link>
+                    <Link to ='/Contact'>
+                    <i className="fa-solid fa-address-book"></i>Contact
+                    </Link>
+                    <Link to ='/Sign Up'>
+                    <i className="fa-solid fa-house"></i>Sign Up
+                    </Link>
+                </li>
+
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
